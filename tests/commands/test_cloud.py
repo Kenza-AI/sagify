fecc8198d5d4d4af167a17c9239be6a4d8d14ab9
pm3310 <p.mitsoulis@gmail.com>
2018-03-31 16:41:04 +0100

    Add cloud train command

--- tests/commands/test_cloud.py
+++ tests/commands/test_cloud.py
@@ -21,7 +21,9 @@ class TestUploadData(object):
             with patch.object(
                     sagify.config.config.ConfigManager,
                     'get_config',
-                    lambda _: Config(aws_profile='sagify', aws_region='us-east-1')
+                    lambda _: Config(
+                        image_name='sagemaker-img', aws_profile='sagify', aws_region='us-east-1'
+                    )
             ):
                 with patch(
                         'sagify.sagemaker.sagemaker.SageMakerClient'
@@ -51,7 +53,9 @@ class TestUploadData(object):
             with patch.object(
                     sagify.config.config.ConfigManager,
                     'get_config',
-                    lambda _: Config(aws_profile='sagify', aws_region='us-east-1')
+                    lambda _: Config(
+                        image_name='sagemaker-img', aws_profile='sagify', aws_region='us-east-1'
+                    )
             ):
                 with patch(
                         'sagify.sagemaker.sagemaker.SageMakerClient'
@@ -85,7 +89,9 @@ class TestUploadData(object):
             with patch.object(
                     sagify.config.config.ConfigManager,
                     'get_config',
-                    lambda _: Config(aws_profile='sagify', aws_region='us-east-1')
+                    lambda _: Config(
+                        image_name='sagemaker-img', aws_profile='sagify', aws_region='us-east-1'
+                    )
             ):
                 with patch(
                         'sagify.sagemaker.sagemaker.SageMakerClient'
@@ -106,5 +112,117 @@ class TestUploadData(object):
                                 '-s', 's3://path-to-data'
                             ]
                         )
+
+        assert result.exit_code == -1
+
+
+class TestTrain(object):
+    def test_train_happy_case(self):
+        runner = CliRunner()
+
+        with patch(
+                'sagify.commands.initialize._get_local_aws_profiles',
+                return_value=['default', 'sagify']
+        ):
+            with patch.object(
+                    sagify.config.config.ConfigManager,
+                    'get_config',
+                    lambda _: Config(
+                        image_name='sagemaker-img', aws_profile='sagify', aws_region='us-east-1'
+                    )
+            ):
+                with patch(
+                        'sagify.sagemaker.sagemaker.SageMakerClient'
+                ) as mocked_sage_maker_client:
+                    instance = mocked_sage_maker_client.return_value
+                    with runner.isolated_filesystem():
+                        runner.invoke(cli=cli, args=['init'], input='my_app\n1\n2\nus-east-1\n')
+                        result = runner.invoke(
+                            cli=cli,
+                            args=[
+                                'cloud', 'train',
+                                '-i', 's3://bucket/input',
+                                '-o', 's3://bucket/output',
+                                '-e', 'ml.c4.2xlarge'
+                            ]
+                        )
+
+                        assert instance.train.assert_called_once
+
+        assert result.exit_code == 0
+
+    def test_train_with_dir_arg_happy_case(self):
+        runner = CliRunner()
+
+        with patch(
+                'sagify.commands.initialize._get_local_aws_profiles',
+                return_value=['default', 'sagify']
+        ):
+            with patch.object(
+                    sagify.config.config.ConfigManager,
+                    'get_config',
+                    lambda _: Config(
+                        image_name='sagemaker-img', aws_profile='sagify', aws_region='us-east-1'
+                    )
+            ):
+                with patch(
+                        'sagify.sagemaker.sagemaker.SageMakerClient'
+                ) as mocked_sage_maker_client:
+                    instance = mocked_sage_maker_client.return_value
+                    with runner.isolated_filesystem():
+                        runner.invoke(
+                            cli=cli, args=['init', '-d', 'src/'], input='my_app\n1\n2\nus-east-1\n'
+                        )
+                        result = runner.invoke(
+                            cli=cli,
+                            args=[
+                                'cloud', 'train',
+                                '-d',
+                                'src/',
+                                '-i', 's3://bucket/input',
+                                '-o', 's3://bucket/output',
+                                '-e', 'ml.c4.2xlarge'
+                            ]
+                        )
+
+                        assert instance.train.assert_called_once
+
+        assert result.exit_code == 0
+
+    def test_train_with_invalid_dir_arg_happy_case(self):
+        runner = CliRunner()
+
+        with patch(
+                'sagify.commands.initialize._get_local_aws_profiles',
+                return_value=['default', 'sagify']
+        ):
+            with patch.object(
+                    sagify.config.config.ConfigManager,
+                    'get_config',
+                    lambda _: Config(
+                        image_name='sagemaker-img', aws_profile='sagify', aws_region='us-east-1'
+                    )
+            ):
+                with patch(
+                        'sagify.sagemaker.sagemaker.SageMakerClient'
+                ) as mocked_sage_maker_client:
+                    instance = mocked_sage_maker_client.return_value
+                    with runner.isolated_filesystem():
+                        runner.invoke(
+                            cli=cli, args=['init', '-d', 'src/'], input='my_app\n1\n2\nus-east-1\n'
+                        )
+                        result = runner.invoke(
+                            cli=cli,
+                            args=[
+                                'cloud', 'train',
+                                '-d',
+                                'invalid_dir/',
+                                '-i', 's3://bucket/input',
+                                '-o', 's3://bucket/output',
+                                '-e', 'ml.c4.2xlarge'
+                            ]
+                        )
+
+                        assert not instance.train.called
 
         assert result.exit_code == -1
