b8a9397d96958a7e898a797ed39f6f8236e67f22
Quinten Kent <berlinquin@gmail.com>
2018-07-12 12:50:27 -0400

    Implement --docker-tag for the build process

--- sagify/template/{{ cookiecutter.module_slug }}/build.sh
+++ sagify/template/{{ cookiecutter.module_slug }}/build.sh
@@ -6,9 +6,10 @@ module_path=$1
 target_dir_name=$2
 dockerfile_path=$3
 requirements_file_path=$4
+tag=$5
 
 image={{ cookiecutter.project_slug }}-img
 
 # Build the docker image
 
-docker build -t ${image} -f ${dockerfile_path} . --build-arg module_path=${module_path} --build-arg target_dir_name=${target_dir_name} --build-arg requirements_file_path=${requirements_file_path}
+docker build -t "${image}:${tag}" -f ${dockerfile_path} . --build-arg module_path=${module_path} --build-arg target_dir_name=${target_dir_name} --build-arg requirements_file_path=${requirements_file_path}
