24f1b8379c6b2bcee20e10ba2222757b537a0a94
Pavlos Mitsoulis Ntompos <pntompos@homeaway.com>
2018-07-29 15:29:09 +0000

    Deployed e5b780c with MkDocs version: 0.17.5

--- sagify/template/{{ cookiecutter.module_slug }}/local_test/deploy_local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-image={{ cookiecutter.project_slug }}-img
-test_path=$1
-tag=$2
-
-docker run -it -v ${test_path}:/opt/ml -p 8080:8080 --rm "${image}:${tag}" serve
--- sagify/template/{{ cookiecutter.module_slug }}/local_test/train_local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-image={{ cookiecutter.project_slug }}-img
-test_path=$1
-tag=$2
-
-docker run -v ${test_path}:/opt/ml --rm "${image}:${tag}" train
