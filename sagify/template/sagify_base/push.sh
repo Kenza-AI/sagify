f457c4a42a8cf19c148e81ae59e436e82180c4bc
Joakim Lazakis <ilazakis@gmail.com>
2020-01-24 19:35:26 +0000

    Do not use AWS profile option if not provided

--- sagify/template/sagify_base/push.sh
+++ sagify/template/sagify_base/push.sh
@@ -9,37 +9,56 @@ image=$6
 
 if [[ ! -z "$role" ]]; then 
     aws configure set profile.${role}.role_arn ${role}
-    if [[ ! -z "$external_id" ]]; then 
+    if [[ ! -z "$external_id" ]]; then
         aws configure set profile.${role}.external_id ${external_id}
     fi
-    
+
     if [[ -z "$profile" ]]; then
         aws configure set profile.${role}.credential_source Ec2InstanceMetadata
     else
         aws configure set profile.${role}.source_profile ${profile}
     fi
     profile=${role}
-fi 
+fi
 
 # Get the account number associated with the current IAM credentials
-account=$(aws sts get-caller-identity --profile ${profile} --query Account --output text)
+
+if [[ ! -z "$profile" ]]; then
+    account=$(aws sts get-caller-identity --profile ${profile} --query Account --output text)
+else
+    account=$(aws sts get-caller-identity --query Account --output text)
+fi
+
 if [ $? -ne 0 ]; then
     exit 255
 fi
 
 # If the repository doesn't exist in ECR, create it.
 fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:${tag}"
-aws ecr describe-repositories --profile ${profile} --region ${region} --repository-names "${image}" > /dev/null 2>&1
+
+if [[ ! -z "$profile" ]]; then
+    aws ecr describe-repositories --profile ${profile} --region ${region} --repository-names "${image}" > /dev/null 2>&1
+else
+    aws ecr describe-repositories --region ${region} --repository-names "${image}" > /dev/null 2>&1
+fi
 
 if [ $? -ne 0 ]; then
     echo "Creating ECR repository"
-    aws ecr create-repository --profile ${profile} --region ${region} --repository-name "${image}" > /dev/null
-else 
+    if [[ ! -z "$profile" ]]; then
+        aws ecr create-repository --profile ${profile} --region ${region} --repository-name "${image}" > /dev/null
+    else
+        aws ecr create-repository --region ${region} --repository-name "${image}" > /dev/null
+    fi
+else
     echo "ECR repository already exists, will push there."
 fi
 
 # Get the login command from ECR and execute it directly
-$(aws ecr get-login --profile ${profile} --region ${region} --no-include-email)
+if [[ ! -z "$profile" ]]; then
+    $(aws ecr get-login --profile ${profile} --region ${region} --no-include-email)
+else
+    $(aws ecr get-login --region ${region} --no-include-email)
+fi
 
 # Push Docker image to ECR
 docker tag ${image}:${tag} ${fullname}
