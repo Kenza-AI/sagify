272188a9dd578ef10d7f94c69e6cf192a80c60be
pm3310 <p.mitsoulis@gmail.com>
2024-03-03 11:45:29 +0000

    0.25.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='sagify',
-    version='0.25.1',
+    version='0.25.2',
     setup_cfg=True,
     python_requires='~=3.7',
     packages=find_packages(where='.'),
